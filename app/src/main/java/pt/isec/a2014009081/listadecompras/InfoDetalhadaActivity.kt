61424e02cf201327c869f69a18887db5da038709
gustavolemos15 <guslemos1515@gmail.com>
2020-12-06 12:22:32 +0000

    Imagem a passar para a info detalhada a funcionar

--- app/src/main/java/pt/isec/a2014009081/listadecompras/InfoDetalhadaActivity.kt
+++ app/src/main/java/pt/isec/a2014009081/listadecompras/InfoDetalhadaActivity.kt
@@ -1,11 +1,13 @@
 package pt.isec.a2014009081.listadecompras
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_edita_produto.*
 import kotlinx.android.synthetic.main.activity_info_detalhada.*
 import kotlinx.android.synthetic.main.activity_info_detalhada.tvCategoria
 import kotlinx.android.synthetic.main.activity_info_detalhada.tvNotas
+import kotlinx.android.synthetic.main.edicao_lista_item.view.*
 
 class InfoDetalhadaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,14 +17,15 @@ class InfoDetalhadaActivity : AppCompatActivity() {
         //recebe produto
         val produto = intent.getSerializableExtra("PRODUTO") as? Produto
 
-
-        //TODO: criar layout para mostrar a informacao do produto
         if (produto != null){
-            //TODO: fazer aqui a ligacao entre o produto e o layout para mostrar a info nas TextViews
             tvDesignacao.text = produto.nome
             tvCategoria.text = produto.categoria
             tvMarca.text = produto.marca
             tvNotas.text = produto.notas
+            if(produto.imagem != null){
+                var foto = BitmapFactory.decodeByteArray(produto.imagem, 0, produto.imagem!!.size);
+                imagemItem.setImageBitmap(foto)
+            }
 
             // Tratameto para a quantidade
             val a = produto.quantidade; val b = " "; val c = produto.unidade; val sb = StringBuilder()
